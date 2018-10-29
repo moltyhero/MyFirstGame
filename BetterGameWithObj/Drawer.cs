237531a7d564433c3a7b6ebae758322bdec5ecfa
Matan Oded <matanoded@gmail.com>
2018-10-29 11:59:54 +0200

    Started oop

---
+++ BetterGameWithObj/Drawer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BetterGameWithObj
+{
+    class Drawer
+    {
+        Texture2D texture;
+        Vector2 position;
+        Rectangle? sourceRectangle;
+        Color color;
+        float rotation;
+        Vector2 origin;
+        float scale;
+        SpriteEffects effects;
+        float layerDepth;
+
+        public Drawer(Texture2D texture, Vector2 position,
+            Rectangle? sourceRectangle, Color color,
+            float rotation, Vector2 origin, float scale,
+            SpriteEffects effects, float layerDepth)
+        {
+
+            this.texture = texture;
+            this.position = position;
+            this.sourceRectangle = sourceRectangle;
+            this.color = color;
+            this.rotation = rotation;
+            this.origin = origin;
+            this.scale = scale;
+            this.effects = effects;
+            this.layerDepth = layerDepth;
+
+        }
+
+        public void Draw()
+        {
+            Game1.spriteBatch.Draw(texture, position, sourceRectangle, color,
+                rotation, origin, scale, effects, layerDepth);
+        }
+    }
+}
--- BetterGameWithObj/Game1.cs
+++ BetterGameWithObj/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace StarterProject
+namespace BetterGameWithObj
 {
     /// <summary>
     /// This is the main type for your game.
@@ -10,12 +10,7 @@ namespace StarterProject
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        Texture2D dragon;
-        Texture2D background;
-        float rot = 0;
-        Vector2 pos;
-
+        public static SpriteBatch spriteBatch;
 
         public Game1()
         {
@@ -44,9 +39,6 @@ namespace StarterProject
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            dragon = Content.Load<Texture2D>("dragon");
-            background = Content.Load<Texture2D>("background");
-            pos = new Vector2(300, 300);
 
             // TODO: use this.Content to load your game content here
         }
@@ -67,8 +59,8 @@ namespace StarterProject
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            rot += 0.1f;
-            pos.X += 10;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
             // TODO: Add your update logic here
 
@@ -84,20 +76,8 @@ namespace StarterProject
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null,
-                Matrix.CreateTranslation(-pos.X, -pos.Y, 0) *
-                Matrix.CreateScale(1f) *
-                Matrix.CreateRotationZ(0) *
-                Matrix.CreateTranslation(0, 0, 0));
-            spriteBatch.Draw(background, Vector2.Zero, null, Color.White, 0,
-                 Vector2.Zero, new Vector2(10f, 10f), SpriteEffects.None, 0);
-            spriteBatch.Draw(dragon, pos, null, Color.White, rot,
-                new Vector2(500, 500), new Vector2(0.1f, 0.1f), SpriteEffects.None, 0);
-            
 
-            spriteBatch.End();
             base.Draw(gameTime);
-
         }
     }
 }
